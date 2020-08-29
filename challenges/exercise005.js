1a437dec49e36791179dc8cb150528b30cd823c4
Jamie Johnstone <j@miej.co>
2020-08-29 17:06:56 +0100

    First attempt at exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,33 +1,45 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+
+  var indexN = nums.findIndex(num => num === n)
+
+  if(indexN == -1 || indexN == (nums.length - 1)) {
+    return null
+  } else {
+    return nums[indexN + 1]
+  }
 };
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  var count = { 0: 0, 1: 0 }
 };
 
-const reverseNumber = n => {
-  if (n === undefined) throw new Error("n is required");
-  // Your code here!
-};
+const reverseNumber = n => parseInt(n.toString().split("").reverse().join(""));
 
-const sumArrays = arrs => {
-  if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
-};
+const sumArrays = arrs => arrs.map(subarrs => subarrs.reduce((a, b) => a + b, 0)).reduce((a, b) => a + b, 0)
 
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  
+  if(arr.length > 1) {
+    var first = arr[0]
+    var last = arr[arr.length - 1]
+    arr[0] = last
+    arr[arr.length - 1] = first
+  }
+
+  return arr
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  
+  var r = haystack.find(property => property.includes(searchTerm))
+
+  return r
 };
 
 const getWordFrequencies = str => {
