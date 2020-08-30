bc46718a73f8c8a3b0ade4b9c30dd2dc1bb06be2
Jamie Johnstone <j@miej.co>
2020-08-30 21:48:45 +0100

    Fix test to use toBe instead of toEqual for string

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -9,13 +9,13 @@ const {
 
 describe("sumMultiples", () => {
     test("check if multiples of 3 and/or 5 are summed", () => {
-        expect(sumMultiples([1,3,6])).toEqual(9)
-        expect(sumMultiples([2,5,10])).toEqual(15)
-        expect(sumMultiples([1,3,10])).toEqual(13)
+        expect(sumMultiples([1,3,6])).toBe(9)
+        expect(sumMultiples([2,5,10])).toBe(15)
+        expect(sumMultiples([1,3,10])).toBe(13)
     })
     test("check if no multiples equals 0", () => {
-        expect(sumMultiples([1,2,7,11])).toEqual(0)
-        expect(sumMultiples([])).toEqual(0)
+        expect(sumMultiples([1,2,7,11])).toBe(0)
+        expect(sumMultiples([])).toBe(0)
     })
     test("check function requires an array", () => {
         expect(() => {
@@ -29,19 +29,19 @@ describe("sumMultiples", () => {
 
 describe("isValidDNA", () => {
     test("check that valid string returns true", () => {
-        expect(isValidDNA("CGTA")).toEqual(true)
-        expect(isValidDNA("CCCC")).toEqual(true)
-        expect(isValidDNA("CGATAGTACAGATA")).toEqual(true)
+        expect(isValidDNA("CGTA")).toBe(true)
+        expect(isValidDNA("CCCC")).toBe(true)
+        expect(isValidDNA("CGATAGTACAGATA")).toBe(true)
     })
     test("check that mixed string return false", () => {
-        expect(isValidDNA("CGPWTA")).toEqual(false)
-        expect(isValidDNA("C20CCC")).toEqual(false)
-        expect(isValidDNA("CGP03GAGATA")).toEqual(false)
+        expect(isValidDNA("CGPWTA")).toBe(false)
+        expect(isValidDNA("C20CCC")).toBe(false)
+        expect(isValidDNA("CGP03GAGATA")).toBe(false)
     })
     test("check that invalid string return false", () => {
-        expect(isValidDNA("ARPW")).toEqual(false)
-        expect(isValidDNA("2039422")).toEqual(false)
-        expect(isValidDNA("!)#K$%I#FJ")).toEqual(false)
+        expect(isValidDNA("ARPW")).toBe(false)
+        expect(isValidDNA("2039422")).toBe(false)
+        expect(isValidDNA("!)#K$%I#FJ")).toBe(false)
     })
     test("check function requires a string", () => {
         expect(() => {
@@ -73,26 +73,26 @@ describe("getComplementaryDNA", () => {
         }).toThrow("str must be a String")
     })
     test("check that a valid DNA string returns valid pairs", () => {
-        expect(getComplementaryDNA("ACTG")).toEqual("TGAC")
-        expect(getComplementaryDNA("TGAC")).toEqual("ACTG")
-        expect(getComplementaryDNA("CCCCCC")).toEqual("GGGGGG")
+        expect(getComplementaryDNA("ACTG")).toBe("TGAC")
+        expect(getComplementaryDNA("TGAC")).toBe("ACTG")
+        expect(getComplementaryDNA("CCCCCC")).toBe("GGGGGG")
     })
 })
 
 describe("isItPrime", () => {
     test("check that prime number returns true", () => {
-        expect(isItPrime(7)).toEqual(true)
-        expect(isItPrime(13)).toEqual(true)
-        expect(isItPrime(23.0)).toEqual(true)
+        expect(isItPrime(7)).toBe(true)
+        expect(isItPrime(13)).toBe(true)
+        expect(isItPrime(23.0)).toBe(true)
     })
     test("check that non-prime number returns false", () => {
-        expect(isItPrime(2)).toEqual(false)
-        expect(isItPrime(14)).toEqual(false)
-        expect(isItPrime(21)).toEqual(false)
-        expect(isItPrime(0)).toEqual(false)
-        expect(isItPrime(1)).toEqual(false)
-        expect(isItPrime(-3)).toEqual(false)
-        expect(isItPrime(-13)).toEqual(false)
+        expect(isItPrime(2)).toBe(false)
+        expect(isItPrime(14)).toBe(false)
+        expect(isItPrime(21)).toBe(false)
+        expect(isItPrime(0)).toBe(false)
+        expect(isItPrime(1)).toBe(false)
+        expect(isItPrime(-3)).toBe(false)
+        expect(isItPrime(-13)).toBe(false)
     })
     test("check function requires a Number", () => {
         expect(() => {
@@ -148,14 +148,14 @@ describe("areWeCovered", () => {
              { name: "Jake", rota: ["Wednesday", "Thursday", "Friday", "Sunday"] },
              { name: "Jeff", rota: ["Thursday", "Friday", "Sunday"] }
          ]
-        expect(areWeCovered(r1, "Monday")).toBe(false) // 1 person working
-        expect(areWeCovered(r1, "Tuesday")).toBe(false) // 2 people working
-        expect(areWeCovered(r1, "Wednesday")).toBe(true) // 3 people working
-        expect(areWeCovered(r1, "Thursday")).toBe(true) // 3 people working
-        expect(areWeCovered(r1, "Friday")).toBe(false) // 2 people working
-        expect(areWeCovered(r1, "Saturday")).toBe(false) // 0 people working
-        expect(areWeCovered(r1, "Sunday")).toBe(true) // 4 people working
-        expect(areWeCovered(r1, "Fruitday")).toBe(false) // aren't validating day names, should fail still
+        expect(areWeCovered(r1, "Monday")).toEqual(false) // 1 person working
+        expect(areWeCovered(r1, "Tuesday")).toEqual(false) // 2 people working
+        expect(areWeCovered(r1, "Wednesday")).toEqual(true) // 3 people working
+        expect(areWeCovered(r1, "Thursday")).toEqual(true) // 3 people working
+        expect(areWeCovered(r1, "Friday")).toEqual(false) // 2 people working
+        expect(areWeCovered(r1, "Saturday")).toEqual(false) // 0 people working
+        expect(areWeCovered(r1, "Sunday")).toEqual(true) // 4 people working
+        expect(areWeCovered(r1, "Fruitday")).toEqual(false) // aren't validating day names, should fail still
     })
 
     test("check function validates staff entries are formatted correctly", () => {
